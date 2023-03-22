d5cf7baf443c88cc3adc50d5a112140736f7e775
dev-hyeonmin <dev.hyeonmin@gmail.com>
2023-03-22 10:22:30 +0900

    nav

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function NavBar () {
+export default function NavBar() {
+    const router = useRouter();
+    
     return (
         <nav>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/about'}>About</Link>
+            <Link className={router.pathname === '/' ? 'active ': ''} href={'/'}>Home</Link>
+            <Link className={router.pathname === '/about' ? 'active' : ''} href={'/about'}>About</Link>
         </nav>
     )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,6 @@
-import NavBar from "@/components/NavBar";
-
-export default function Hello() {
+export default function About () {
     return (
         <>
-            <NavBar />
             <h1>About</h1>
         </>
     )
