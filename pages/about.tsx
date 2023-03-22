464c82b3b7187af0a8a68dec9a21c376e962ce7f
dev-hyeonmin <dev.hyeonmin@gmail.com>
2023-03-22 10:34:28 +0900

    Seo(Header - Halmet) & Layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,9 @@
-export default function About () {
+import Seo from '../components/Seo';
+
+export default function About() {
     return (
         <>
+            <Seo title="About" />
             <h1>About</h1>
         </>
     )
