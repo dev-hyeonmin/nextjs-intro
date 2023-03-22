e0d8ae7c56cf1a6f0bbb363960a91b0da802737e
dev-hyeonmin <dev.hyeonmin@gmail.com>
2023-03-22 12:11:11 +0900

    Movies

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from '../styles/NavBar.module.css';
 
 export default function NavBar() {
     const router = useRouter();
     
     return (
-        <nav>
-            <Link className={router.pathname === '/' ? 'active ': ''} href={'/'}>Home</Link>
-            <Link className={router.pathname === '/about' ? 'active' : ''} href={'/about'}>About</Link>
+        <nav className={styles.nav}>
+            <Link className={router.pathname === '/' ? styles.active : ''} href={'/'}>Home</Link>
+            <Link className={router.pathname === '/about' ? styles.active : ''} href={'/about'}>About</Link>
         </nav>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
+import styles from '../styles/index.module.css';
+
+interface movieProps {
+  id: number;
+  original_title: string;
+  poster_path: string;
+}
 
 export default function Home() {
+  const [movies, setMovies] = useState<movieProps[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(`/api/movies`)
+      ).json();
+
+      console.log(results);
+      setMovies(results);
+    })();
+  }, []);
+
+
   return (
     <>
       <Seo title="Home" />
       <h1>Home</h1>
+      
+      <div>
+        <ul className={styles.movie_list}>
+          {movies.map((movie) => 
+            <li key={movie.id}>
+              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+              {movie.original_title}
+            </li>
+          )}
+        </ul>
+      </div>
     </>
   )
 }
